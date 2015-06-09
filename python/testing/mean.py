fc87c9130bffcd85ee97fa70a13b834d426c376d
Sarah Stevens <sstevens2@wisc.edu>
2015-06-09 18:01:59 -0500

    Removed obviously un-needed material for git workshop

--- python/testing/mean.py
+++
@@ -1,9 +0,0 @@
-def mean(vals):
-    try :
-        total = sum(vals)
-        length = len(vals)
-    except TypeError :
-        raise TypeError("The list contained non-numeric elements.")
-    except :
-        print "Something unknown happened with the list."
-    return total/length
